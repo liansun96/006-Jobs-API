3af97368f4874adb09f89dd192675419a1cc3da3
liansun96 <liansun0203@gmail.com>
2025-01-21 18:05:05 +0630

    update mongoose validation error

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require("http-status-codes")
 const Job = require("../models/Job")
-const { CustomAIPError } = require("../errors")
+const { CustomAIPError, BadRequestError } = require("../errors")
 const notFoundError = require("../errors/not-found")
 
 const getAllJobs = async(req, res) => {
@@ -27,19 +27,23 @@ const createJob= async(req, res) => {
 }
 
 const updateJob = async(req, res) => {
-    const {body : {company , position } , params : {id : jobId}} = req
+    const {body : {company , position } , params : {id : jobId} , user : {userId}} = req
 
-    const job = await Job.findByIdAndUpdate({_id : jobId } , req.body , {new : true})
+    if(!company || !position){
+        throw new BadRequestError('Company or postion fields cannot be empty')
+    }
+
+    const job = await Job.findByIdAndUpdate({_id : jobId , createdBy : userId} , req.body , {new : true , runValidators : true})
     if(!job){
         throw new notFoundError(`No job with id : ${jobId}`)
     }
 
     res.status(StatusCodes.OK).json({job})
 }
-
+ 
 const deleteJob = async(req, res) => {
   const {user : {userId} , params : {id : jobId}} = req
-
+c
   const job = await Job.findByIdAndDelete({_id : jobId})
   if(!job){
     throw new notFoundError(`No job with id : ${jobId}`)
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,11 +2,23 @@ const { StatusCodes } = require("http-status-codes")
 const { CustomAIPError } = require("../errors")
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+
+    let customError = {
+        statusCode : err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR , 
+        msg : err.message || 'Something Went Wrong Please Try Again Later'
+    }
+
     if(err instanceof CustomAIPError){
         return res.status(err.statusCode).json({msg:err.message})
     }
 
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({msg : err.message})
+    if(err.code && err.code === 11000 ){
+        customError.msg = `Duplicate value enter for ${Object.keys(err.keyValue)} field , Please chose another value`
+        customError.statusCode = 400
+    }
+
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err)
+    return res.status(customError.statusCode).json({msg : customError.msg})
 }
 
 module.exports = errorHandlerMiddleware 
